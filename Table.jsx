99b0a6582e98443e6e9086b8c10570ecb427a6b0
JoaquinRodriguez <joacorod@live.com>
2024-04-23 13:56:59 -0300

    Practica Obligatoria Ejercicio 2

---
+++ Table.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Table.css';
+
+const Table = ({ netIncomes }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Marca</th>
+          <th>Ingreso Neto</th>
+        </tr>
+      </thead>
+      <tbody>
+        {netIncomes.map((item, index) => (
+          <tr key={index}>
+            <td>{item.brand}</td>
+            <td>{item.income}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
+
